a6e56fc614166481797d4fc36563a2a07f790778
sasata299 <sasata299@livedoor.com>
2013-11-03 15:16:51 +0900

    first commit

---
+++ public/javascripts/client.js
@@ -0,0 +1,12 @@
+$(function(){
+  var socket = io.connect('/');
+  socket.on('msg', function (data) {
+    $('#list').prepend('<li>' + data.date + ' : ' + data.message + '</li>');
+  });
+
+  $('#btn').on('click', function(){
+    var message = $('#message').val()
+    socket.emit('msg', { message : message });
+    $('#message').val('');
+  });
+});
